af9cc98cacb4f9857afcadd42bdf88104a300e91
agusguccy <50383854+agusguccy@users.noreply.github.com>
2019-05-26 09:46:02 -0300

    Página de perfil

---
+++ profile.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Perfil</title>
+  <!-- <link rel="stylesheet" href="css/styles.css"> -->
+</head>
+
+<body>
+
+  <div id="w">
+    <div id="content" class="clearfix">
+      <div id="userphoto"><img src="" alt=""></div>
+      <h1>Mi Perfil</h1>
+
+      <nav id="profiletabs">
+        <ul class="clearfix">
+          <li><a href="#bio" class="sel">Bio</a></li>
+          <li><a href="#historial">Historial</a></li>
+          <li><a href="#configuración">Configuración</a></li>
+        </ul>
+      </nav>
+
+      <section id="bio">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      </section>
+
+      <section id="historial" class="hidden">
+        <p>Productos que visitaste recientemente:</p>
+
+        <p class="activity">@10:15PM - Cerveza Blonde barril 50 lts</p>
+
+        <p class="activity">@9:50PM - Cerveza Session IPA growlers 1.9 lts</p>
+
+      </section>
+
+
+      <section id="configuración" class="hidden">
+        <p>Edita tu configuración:</p>
+
+        <p class="setting"><span>E-mail: <img src="images/edit.png" alt=></span> blabla@gmail.com</p>
+
+        <p class="setting"><span>Status del Perfil: <img src="images/edit.png" alt=></span> Público</p>
+
+        <p class="setting"><span>Cuentas Conectadas: <img src="images/edit.png" alt=></span> Ninguna</p>
+      </section>
+    </div>
+  </div>
+
+</body>
+</html>
