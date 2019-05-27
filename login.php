d908523a36e33dc35473fd438a9a24e07d0c8606
HoracioCalleja <50384330+HoracioCalleja@users.noreply.github.com>
2019-05-27 18:35:44 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+<link rel="stylesheet" href="login.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<form class="login" action="index.html" method="post">
+<div class="container">
+
+
+<div class="container-login">
+
+<h1>BIENVENIDA/O</h1>
+
+<div class="ingresar-datos">
+
+      <label  for="">Email: </label>
+      <input class="casillero"  type="email" id="email" name="email" value="">
+
+</div>
+
+<div class="ingresar-datos">
+    <label style= for="">Contraseña: </label>
+    <input class="casillero"  type="password" id="contraseña" name="contraseña" value="">
+</div>
+<div  class="container-boton">
+   <input class="casillero" type="submit" name="" id="boton" value="LOGIN">
+</div>
+
+<div class="container-olvidaste">
+    <p> <a  href="#"> ¿Olvidaste tu contraseña? Clickea acá</a> </p>
+</div>
+
+
+
+</div>
+</div>
+</form>
+
+  </body>
+</html>
