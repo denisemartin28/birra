b592964d3372f7383b611535b594b10148c23c84
Denise Martin Caldareri <denise.martincaldareri@swissmedical.com.ar>
2019-05-27 14:06:53 -0300

    header

--- header.php
+++ header.php
@@ -2,29 +2,74 @@
 <?php
 $navbar=[
   "Home"=>"home.php",
-  "Birras"=>"birras.php",
+  "Birras"=>"list-products.php",
   "FAQ"=>"faq.php",
   "Ingresá"=>"login.php",
-  "Registrate"=>"registrate.php"
+  "Registrate"=>"register.php",
 ]
  ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="stylesheet.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title></title>
   </head>
   <body>
-    <div class="container">
-      <header>
-        <nav class="navbar">
-          <ul>
-            <?php foreach ($navbar as $pagina => $link): ?>
-            <li><a href="<?php echo $link; ?>"><?php echo $pagina; ?></a></li>
-            <?php endforeach; ?>
-          </ul>
+  <div class="containerbody">
+    <div class="jumbotron">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
 
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+          <div class="item active">
+              <img  src=img\growlacb.png alt="">
+          </div>
+          <div class="item">
+              <img src=img\descarga.jpg alt="">
+          </div>
+          <div class="item">
+              <img src=img\imagen3.jpg alt="">
+          </div>
+      </div>
+    </div>
+
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+                <?php foreach ($navbar as $titulo => $link) : ?>
+      						<li><a href="<?php echo $link; ?>"> <?php echo $titulo; ?> </a></li>
+      					<?php endforeach; ?>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Perfil</a></li>
+
+              </ul>
+            </div>
+          </div>
         </nav>
+        </div><br>
       </header>
     </div>
+
+  </div>
   </body>
 </html>
--- home.php
+++ home.php
@@ -2,13 +2,17 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Growla/title>
-    <link rel="stylesheet" href="css/stylesheet.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="stylesheet.css">
+    <title>Growla</title>
+    <?php include("header.php") ?>
   </head>
   <body>
-    <div class="contenedor">
+    <div class="containerbody">
 
-    </div>
 
+        <h1>Bienvenido a Growla</h1>
+
+      </div>
   </body>
 </html>
