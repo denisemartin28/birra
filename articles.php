2a459a5db5817574953fef588a6e5b94d7d18606
AlexisGBarrientos <50384373+AlexisGBarrientos@users.noreply.github.com>
2019-06-01 14:00:36 -0300

    Update articles.php

--- articles.php
+++ articles.php
@@ -1,56 +1,79 @@
 
-<?php
-$productsList = [
-        [
-          'id' => 1,
-          'image' =>,
-          'title' => "PORTER",
-          'description' => 'Cerveza oscura perteneciente a la familia de las ALE, de moderado bajo aroma de malta tostada, de pan y bizcocho, con un suave olor torrefacto y un tono a chocolate.
-          CARACTERISTICAS:
-          Alcohol –  5.5%.
-          Amargor – 35 IBUs.',
-          'ranking' =>
-        ],
-        [
-          'id' => 2,
-          'image' =>,
-          'title' => 'BLONDE ALE',
-          'description' => 'Cerveza dorada de fermentación alta, perteneciente a la familia de las ALE, de moderada intensidad que tiene una complejidad sutil frutal-especiada a levadura belga, un poco de sabor a malta dulce y un final seco.
-            CARACTERISTICAS:
-          Alcohol – 3.8%.
-          Amargor – 28 IBUs.',
-          'ranking' =>,
-        ],
-        [
-          'id' => 3,
-          'image' =>,
-          'title' => 'OKCTOBERFEST',
-          'description' => 'El sabor a malta inicial a menudo sugiere dulzura, pero de acabado moderadamente seco. El complejo y único carácter maltoso a menudo incluye notas a pan y tostado. El amargor procedente del lúpulo es moderado, a pesar de que su sabor sea de bajo a imperceptible. Los lúpulos, sin embargo, otorgan a la cerveza el equilibrio suficiente para que no predomine el dulzor de la malta.
-            CARACTERISTICAS:
-          Alcohol – 6.2%.
-          Amargor –  30 IBUs.',
-          'ranking' =>,
-        ],
-        [
-          'id' => 4,
-          'image' =>,
-          'title' => 'IPA',
-          'description' => 'Cerveza de fermentación alta y calificada como pálida, para diferenciarla de las características sobre las Brown, Stout o Porters. Presentan un color pálido anaranjado similar al ámbar de menor o mayor intensidad y poseen un mayor aporte de lúpulo que proporciona un mayor grado de amargor, aparte de una serie de matices aromáticos y de sabor particular.
-            CARACTERISTICAS:
-          Alcohol – 5.1%.
-          Amargor –  40 IBUs.',
-          'ranking' =>,
-        ],
-        [
-          'id' =>5,
-          'image' =>,
-          'title' => 'SESION IPA',
-          'description' => 'Cerveza fácil de beber pero sin dejar de lado las características de la IPA, tiene un sabor algo más seco que las Pale Ales, aunque es innegable la gran similitud entre ambas.  Posee un cuerpo sumamente ligero y junto al lúpulo viene el intenso aroma, pero sin el amargor pronunciado ni el alto porcentaje de alcohol.
-            CARACTERISTICAS:
-          Alcohol – 4.5%.
-          Amargor – 50 IBUs.',
-          'ranking' =>,
-        ],
 
-]
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/styles-articles.css">
+    <title>Articles</title>
+  </head>
+  <body>
+    <?php require("header.php") ?>
+    <section class="container-products">
+
+      <article class="" id="1">
+        <img src="img/IMG-20190523-WA0105.jpg" alt="cerveza blonde-ale">
+        <h3>BLONDE ALE</h3>
+        <p>Cerveza dorada de fermentación alta, perteneciente a la familia de las ALE, de moderada intensidad que tiene una complejidad sutil frutal-especiada a levadura belga, un poco de sabor a malta dulce y un final seco.<br><br>
+          CARACTERISTICAS:
+          <ul>
+            <li><i>Alcohol – 3.8%.</i></li>
+            <li><i>Amargor – 28 IBUs.</i></li>
+          </ul>
+        </p>
+      </article>
+
+      <article class="" id="2">
+        <img src="img/IMG-20190523-WA0101.jpg" alt="cerveza ipa">
+        <h3>IPA</h3>
+        <p>Cerveza de fermentación alta y calificada como pálida, para diferenciarla de las características sobre las Brown, Stout o Porters. Presentan un color pálido anaranjado similar al ámbar de menor o mayor intensidad y poseen un mayor aporte de lúpulo que proporciona un mayor grado de amargor, aparte de una serie de matices aromáticos y de sabor particular.<br><br>
+          CARACTERISTICAS:
+          <ul>
+            <li><i>Alcohol – 5.1%.</i></li>
+            <li><i>Amargor –  40 IBUs.</i></li>
+          </ul>
+        </p>
+      </article>
+
+      <article class="" id="3">
+        <img src="img/IMG-20190523-WA0104.jpg" alt="cerveza Session">
+        <h3>SESSION IPA</h3>
+        <p>Cerveza fácil de beber pero sin dejar de lado las características de la IPA, tiene un sabor algo más seco que las Pale Ales, aunque es innegable la gran similitud entre ambas.  Posee un cuerpo sumamente ligero y junto al lúpulo viene el intenso aroma, pero sin el amargor pronunciado ni el alto porcentaje de alcohol.<br><br>
+          CARACTERISTICAS:
+          <ul>
+            <li><i>Alcohol – 4.5%.</i></li>
+            <li><i>Amargor – 50 IBUs.</i></li>
+          </ul>
+        </p>
+      </article>
+
+      <article class="" id="4">
+        <img src="img/IMG-20190523-WA0103.jpg" alt="cerveza Oktoberfest">
+        <h3>OKCTOBERFEST</h3>
+        <p>El sabor a malta inicial a menudo sugiere dulzura, pero de acabado moderadamente seco. El complejo y único carácter maltoso a menudo incluye notas a pan y tostado. El amargor procedente del lúpulo es moderado, a pesar de que su sabor sea de bajo a imperceptible. Los lúpulos, sin embargo, otorgan a la cerveza el equilibrio suficiente para que no predomine el dulzor de la malta.<br><br>
+          CARACTERISTICAS:
+          <ul>
+            <li><i>Alcohol – 6.2%.</i></li>
+            <li><i>Amargor –  30 IBUs.</i></li>
+          </ul>
+        </p>
+      </article>
+
+      <article class="" id="5">
+        <img src="img/IMG-20190523-WA0094.jpg" alt="cerveza porter">
+        <h3>PORTER</h3>
+        <p>Cerveza oscura perteneciente a la familia de las ALE, de moderado bajo aroma de malta tostada, de pan y bizcocho, con un suave olor torrefacto y un tono a chocolate.<br><br>
+        CARACTERISTICAS:
+          <ul>
+            <li><i>Alcohol –  5.5%.</i></li>
+            <li><i>Amargor – 35 IBUs.</i></li>
+          </ul>
+        </p>
+
+      </article>
+    </section>
+
+    <?php require("footer.php") ?>
+  </body>
+</html>
