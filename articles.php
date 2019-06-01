365d887ff2ab20498c5a41b6a39f4d502d8e33b7
AlexisGBarrientos <50384373+AlexisGBarrientos@users.noreply.github.com>
2019-06-01 14:03:29 -0300

    Update articles.php

--- articles.php
+++ articles.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/styles-articles.css">
+    <link rel="stylesheet" href="">
     <title>Articles</title>
   </head>
   <body>
