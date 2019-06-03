811263542cdd417ff44b5e36597316d34e760cda
Denise Martin Caldareri <denise.martincaldareri@swissmedical.com.ar>
2019-06-03 10:01:43 -0300

    home responsive

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
   <body>
     <?php require("header.php") ?>
     <div class="home">
-    <img src="img\logoblanco.png" alt="logo completo" class="logo-home">
+    <img class="growla" src="img\logonegropaint.png" alt="logo completo">
     <h1>Somos Growla, una empresa que crece sobre dos pilares fundamentales: el amor por la birra y la amistad. </h1>
     </div>
     <?php require("footer.php") ?>
