b5fbbf4422fa295da60a7b9ef60c5646c6bce33a
AlexisGBarrientos <50384373+AlexisGBarrientos@users.noreply.github.com>
2019-06-03 13:05:46 -0300

    Update articles.php

--- articles.php
+++ articles.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -18,13 +17,13 @@
         <img src="img/IMG-20190523-WA0105.jpg" alt="cerveza blonde-ale">
         <div class="description">
           <h3>BLONDE ALE</h3>
-            <p>Cerveza dorada de fermentación alta, perteneciente a la familia de las ALE, de moderada intensidad que tiene una complejidad sutil frutal-especiada a levadura belga, un poco de sabor a malta dulce y un final seco.<br>
+            <p>Cerveza dorada de fermentación alta, perteneciente a la familia de las ALE, de moderada intensidad que tiene una complejidad sutil frutal-especiada a levadura belga, un poco de sabor a malta dulce y un final seco.<br><br>
               CARACTERISTICAS:
               <ul>
                 <li><i>Alcohol – 3.8%.</i></li>
                 <li><i>Amargor – 28 IBUs.</i></li><br>
-                <i class="fas fa-beer"></i> 500cc.<br>
-                <i class="fas fa-beer"></i> 1lt.
+                <i class="fas fa-beer"> 500cc.</i><br>
+                <i class="fas fa-beer"> 1lt.</i>
               </ul>
             </p>
         </div>
@@ -34,13 +33,13 @@
         <img src="img/IMG-20190523-WA0101.jpg" alt="cerveza ipa">
         <div class="description">
           <h3>IPA</h3>
-            <p>Cerveza de fermentación alta y calificada como pálida, para diferenciarla de las características sobre las Brown, Stout o Porters. Presentan un color pálido anaranjado similar al ámbar de menor o mayor intensidad y poseen un mayor aporte de lúpulo que proporciona un mayor grado de amargor, aparte de una serie de matices aromáticos y de sabor particular.<br>
+            <p>Cerveza de fermentación alta y calificada como pálida, para diferenciarla de las características sobre las Brown, Stout o Porters. Presentan un color pálido anaranjado similar al ámbar de menor o mayor intensidad y poseen un mayor aporte de lúpulo que proporciona un mayor grado de amargor, aparte de una serie de matices aromáticos y de sabor particular.<br><br>
               CARACTERISTICAS:
               <ul>
                 <li><i>Alcohol – 5.1%.</i></li>
                 <li><i>Amargor –  40 IBUs.</i></li><br>
-                <i class="fas fa-beer"></i> 500cc.<br>
-                <i class="fas fa-beer"></i> 1lt.
+                <i class="fas fa-beer"> 500cc.</i><br>
+                <i class="fas fa-beer"> 1lt.</i>
               </ul>
             </p>
         </div>
@@ -50,13 +49,13 @@
         <img src="img/IMG-20190523-WA0104.jpg" alt="cerveza Session">
         <div class="description">
           <h3>SESSION IPA</h3>
-            <p>Cerveza fácil de beber pero sin dejar de lado las características de la IPA, tiene un sabor algo más seco que las Pale Ales, aunque es innegable la gran similitud entre ambas.  Posee un cuerpo sumamente ligero y junto al lúpulo viene el intenso aroma, pero sin el amargor pronunciado ni el alto porcentaje de alcohol.<br>
+            <p>Cerveza fácil de beber pero sin dejar de lado las características de la IPA, tiene un sabor algo más seco que las Pale Ales, aunque es innegable la gran similitud entre ambas.  Posee un cuerpo sumamente ligero y junto al lúpulo viene el intenso aroma, pero sin el amargor pronunciado ni el alto porcentaje de alcohol.<br><br>
               CARACTERISTICAS:
               <ul>
                 <li><i>Alcohol – 4.5%.</i></li>
                 <li><i>Amargor – 50 IBUs.</i></li><br>
-                <i class="fas fa-beer"></i> 500cc.<br>
-                <i class="fas fa-beer"></i> 1lt.
+                <i class="fas fa-beer"> 500cc.</i><br>
+                <i class="fas fa-beer"> 1lt.</i>
               </ul>
             </p>
         </div>
@@ -66,13 +65,13 @@
         <img src="img/IMG-20190523-WA0103.jpg" alt="cerveza Oktoberfest">
         <div class="description">
           <h3>OKCTOBERFEST</h3>
-            <p>El sabor a malta inicial a menudo sugiere dulzura, pero de acabado moderadamente seco. El complejo y único carácter maltoso a menudo incluye notas a pan y tostado. El amargor procedente del lúpulo es moderado, a pesar de que su sabor sea de bajo a imperceptible. Los lúpulos, sin embargo, otorgan a la cerveza el equilibrio suficiente para que no predomine el dulzor de la malta.<br>
+            <p>El sabor a malta inicial a menudo sugiere dulzura, pero de acabado moderadamente seco. El complejo y único carácter maltoso a menudo incluye notas a pan y tostado. El amargor procedente del lúpulo es moderado, a pesar de que su sabor sea de bajo a imperceptible. Los lúpulos, sin embargo, otorgan a la cerveza el equilibrio suficiente para que no predomine el dulzor de la malta.<br><br>
               CARACTERISTICAS:
               <ul>
                 <li><i>Alcohol – 6.2%.</i></li>
                 <li><i>Amargor –  30 IBUs.</i></li><br>
-                <i class="fas fa-beer"></i> 500cc.<br>
-                <i class="fas fa-beer"></i> 1lt.
+                <i class="fas fa-beer"> 500cc.</i><br>
+                <i class="fas fa-beer"> 1lt.</i>
               </ul>
             </p>
         </div>
@@ -82,13 +81,13 @@
         <img src="img/IMG-20190523-WA0094.jpg" alt="cerveza porter">
         <div class="description">
           <h3>PORTER</h3>
-            <p>Cerveza oscura perteneciente a la familia de las ALE, de moderado bajo aroma de malta tostada, de pan y bizcocho, con un suave olor torrefacto y un tono a chocolate.<br>
+            <p>Cerveza oscura perteneciente a la familia de las ALE, de moderado bajo aroma de malta tostada, de pan y bizcocho, con un suave olor torrefacto y un tono a chocolate.<br><br>
             CARACTERISTICAS:
               <ul>
                 <li><i>Alcohol –  5.5%.</i></li>
                 <li><i>Amargor – 35 IBUs.</i></li><br>
-                <i class="fas fa-beer"></i> 500cc.<br>
-                <i class="fas fa-beer"></i> 1lt.
+                <i class="fas fa-beer"> 500cc.</i><br>
+                <i class="fas fa-beer"> 1lt.</i>
               </ul>
             </p>
         </div>
