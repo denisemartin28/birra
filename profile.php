b15d2ffa023ba21528a2b99bb2dca9ede04c0460
agusguccy <50383854+agusguccy@users.noreply.github.com>
2019-06-04 10:13:07 -0300

    Actualización de la página del Perfil del usuario

--- profile.php
+++ profile.php
@@ -3,50 +3,55 @@
 <head>
   <meta charset="utf-8">
   <title>Perfil</title>
-  <!-- <link rel="stylesheet" href="css/styles.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="css/css-profile.css">
 </head>
 
 <body>
+  <?php require_once "header.php" ?>
+  <div class="container" style="align-items: center;">
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-6">
+            <div class="well well-sm" id="well">
+                <div class="row" id="row">
+                    <div class="col-sm-6 col-md-4">
+                        <img src="img/avatar.jpg"  alt="imagen de usuario" class="img-rounded img-responsive" />
+                    </div>
+                    <div class="col-sm-6 col-md-8">
+                        <h4>
+                            Pepito Perez</h4>
 
-  <div id="w">
-    <div id="content" class="clearfix">
-      <div id="userphoto"><img src="" alt=""></div>
-      <h1>Mi Perfil</h1>
+                        <small>Birra fan lvl 9000 <i class="fas fa-beer"></i></small>
+                      <br />
+                        <p>
+                        </br>
+                            <i class="fas fa-envelope-open"></i> pepitoelmejor@gmail.com
+                            <br />
+                            <i class="fab fa-facebook"></i> Pepito Perez
+                            <br />
+                            <i class="fab fa-instagram"></i> pepitoamalabirraok</p>
 
-      <nav id="profiletabs">
-        <ul class="clearfix">
-          <li><a href="#bio" class="sel">Bio</a></li>
-          <li><a href="#historial">Historial</a></li>
-          <li><a href="#configuración">Configuración</a></li>
-        </ul>
-      </nav>
-
-      <section id="bio">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-      </section>
-
-      <section id="historial" class="hidden">
-        <p>Productos que visitaste recientemente:</p>
-
-        <p class="activity">@10:15PM - Cerveza Blonde barril 50 lts</p>
-
-        <p class="activity">@9:50PM - Cerveza Session IPA growlers 1.9 lts</p>
-
-      </section>
-
-
-      <section id="configuración" class="hidden">
-        <p>Edita tu configuración:</p>
-
-        <p class="setting"><span>E-mail: <img src="images/edit.png" alt=></span> blabla@gmail.com</p>
-
-        <p class="setting"><span>Status del Perfil: <img src="images/edit.png" alt=></span> Público</p>
-
-        <p class="setting"><span>Cuentas Conectadas: <img src="images/edit.png" alt=></span> Ninguna</p>
-      </section>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-primary">
+                                Editar</button>
+                            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                                <span class="caret"></span><span class="sr-only">Social</span>
+                            </button>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="">Configuración</a></li>
+                                <li class="divider"></li>
+                                <li><a href="">Cerrar Sesión</a></li>
+                           </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
 
+<?php require_once "footer.php" ?>
 </body>
 </html>
