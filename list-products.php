a626e26e1a5bfeb764e9aab98113144b682a35c3
agusguccy <50383854+agusguccy@users.noreply.github.com>
2019-06-04 10:11:49 -0300

    Actualización del archivo del Listado de Productos

--- list-products.php
+++ list-products.php
@@ -42,6 +42,7 @@
 
 
 ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -49,34 +50,34 @@
 		<title>Listado de Productos</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="stylesheet" href="stylesheeet.css">
-		<link rel="stylesheet" href="css-list-products.css">
-
+		<link rel="stylesheet" href="css/stylesheet.css">
+		<link rel="stylesheet" href="css/css-list-products.css">
 
 
 	</head>
-	<body>
+	<body id="body">
 		<?php require_once "header.php" ?>
     <div class="container">
   <div class="row">
-    <div class="col-sm-4">
+    <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-4">
       <div class="panel panel-default text-center mt-3" >
         <div class="panel-heading panel-title">BLONDE</div>
-        <div class="panel-body"><img src="img/IMG-20190523-WA0105.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-        <div class="panel-footer"><a class="more stretched-link" href="articles.php">Ver más</a></div>
+				<!-- La idea de las imágenes es poder ir a sacar fotos a la fábrica, para que sean originales. Por el momento por no haber podido ir agregamos fotos de internet. -->
+        <div class="panel-body"><img src="img/blonde2.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer"><a class="more" class="stretched-link" href="articles.php">Ver más</a></div>
       </div>
     </div>
-    <div class="col-sm-4">
+    <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-4">
       <div class="panel panel-default text-center mt-3">
-        <div class="panel-heading">IPA</div>
-        <div class="panel-body"><img src="img/IMG-20190523-WA0105.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-heading panel-title">IPA</div>
+        <div class="panel-body"><img src="img/ipa2.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer" class="footer"><a class="more stretched-link" href="articles.php">Ver más</a></div>
       </div>
     </div>
-    <div class="col-sm-4">
+    <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-4">
       <div class="panel panel-default text-center mt-3">
-        <div class="panel-heading">SESSION IPA</div>
-        <div class="panel-body"><img src="img/IMG-20190523-WA0105.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-heading panel-title">SESSION IPA</div>
+        <div class="panel-body"><img src="img/sessionipa2.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a class="more stretched-link" href="articles.php">Ver más</a></div>
       </div>
     </div>
@@ -85,17 +86,17 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-sm-4">
+    <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-4">
       <div class="panel panel-default text-center mt-3">
-        <div class="panel-heading">OKTOBERFEST</div>
-        <div class="panel-body"><img src="img/IMG-20190523-WA0105.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-heading panel-title">OKTOBERFEST</div>
+        <div class="panel-body"><img src="img/oktoberfest2.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a class="more stretched-link" href="articles.php">Ver más</a></div>
       </div>
     </div>
-    <div class="col-sm-4">
+    <div class="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-4">
       <div class="panel panel-default text-center mt-3">
-        <div class="panel-heading">PORTER</div>
-        <div class="panel-body"><img src="img/IMG-20190523-WA0105.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-heading panel-title">PORTER</div>
+        <div class="panel-body"><img src="img/porter2.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
         <div class="panel-footer"><a class="more stretched-link" href="articles.php">Ver más</a></div>
       </div>
     </div>
