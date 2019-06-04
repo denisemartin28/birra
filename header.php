78fa2b85a4a1f43a6c7ed24127a740276ea471b3
Denise Martin Caldareri <denise.martincaldareri@swissmedical.com.ar>
2019-06-04 12:13:49 -0300

    cambios denu

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ $navbar=[
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="css\stylesheet.css">
     <title></title>
   </head>
   <body>
